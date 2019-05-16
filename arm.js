dff6dfb91ec38a97d9523b51927a58f78c4c6cc8
smitpatel2105 <34638870+smitpatel2105@users.noreply.github.com>
2019-05-16 15:41:38 -0400

    jhgyut

--- arm.js
+++ arm.js
@@ -1,18 +1,12 @@
 document.write("hi");
 
-var i=1;
-var j=5;
-var k=3;
-var arm = (Math.pow(i,3) + Math.pow(j,3) + Math.pow(k,3));
-var number = (i*100 + j*10 +  k);
-if (arm == plus) 
-{     
-	console.log(number + "number is armstrong");
+var i = 1;
+var j = 5;
+var k = 3;
+var arm = Math.pow(i, 3) + Math.pow(j, 3) + Math.pow(k, 3);
+var number = i * 100 + j * 10 + k;
+if (arm == plus) {
+  console.log(number + "number is armstrong");
+} else {
+  console.log(number + "number is not armstrong");
 }
-else
-{
-	console.log(number + "number is not armstrong");
-}
-
-
-demo line.please delet it
